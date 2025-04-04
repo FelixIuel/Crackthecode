ffd99b028d49c15a5dff30c6c402a673ef866cfc
ChocZeChoc <nathansl.littlefield@outlook.com>
2025-04-04 14:48:07 +0200

    Merged?

--- src/ExplanationPage.js
+++ src/ExplanationPage.js
@@ -4,15 +4,12 @@ function ExplanationPage() {
   return (
     <div>
       <h1>How to Play</h1>
-<<<<<<< Updated upstream
       <p>Here’s an explanation of the game... this is a test</p>
-=======
       <p>Here’s an explanation of the game...</p>
       <div className='Something-pic'>
       <img src="/Something.png" alt="Something" className="Something" />
       </div>
       
->>>>>>> Stashed changes
       {/* You can add a detailed game explanation here */}
     </div>
   );
